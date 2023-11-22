aa057b6f429901fec67705219e73819664a7e716
theri <therigasmazurets@gmail.com>
2023-11-22 20:36:41 +0200

    add corountines

--- app/src/main/java/com/mazurets/gachivoice/MainActivity.kt
+++ app/src/main/java/com/mazurets/gachivoice/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.mazurets.gachivoice
 
+import android.graphics.Color
 import android.media.MediaPlayer
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import com.mazurets.gachivoice.databinding.ActivityMainBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
         val buttonIds = (1..21).map { resources.getIdentifier("button_voice$it", "id", packageName) }
         val mediaPlayerIds = (1..21).map { resources.getIdentifier("volume$it", "raw", packageName) }
         val buttons = buttonIds.map { findViewById<Button>(it) }
@@ -20,6 +33,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        GlobalScope.launch(Dispatchers.Main) {
+            while (true) {
+                binding.textMainText.text = "G"
+                delay(1000L)
+                binding.textMainText.text = "GA"
+                delay(1000L)
+                binding.textMainText.text = "GAC"
+                delay(1000L)
+                binding.textMainText.text = "GACH"
+                delay(1000L)
+                binding.textMainText.text = "GACHI"
+                delay(1000L)
+                binding.textMainText.text = "GACHI V"
+                delay(1000L)
+                binding.textMainText.text = "GACHI VO"
+                delay(1000L)
+                binding.textMainText.text = "GACHI VOI"
+                delay(1000L)
+                binding.textMainText.text = "GACHI VOIC"
+                delay(1000L)
+                binding.textMainText.text = "GACHI VOICE"
+                delay(1000L)
+                binding.textMainText.text = ""
+            }
+        }
     }
 }
 
